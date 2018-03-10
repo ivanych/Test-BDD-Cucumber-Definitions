7e49081425825fe7ba644462bc6cd5323d7fe2c4
Mikhail Ivanov <m.ivanych@gmail.com>
2018-03-10 23:17:18 +0300

    The TBCD module has been added. The module loads all the step definitions at once.

---
+++ lib/Test/BDD/Cucumber/Definitions/TBCD/In.pm
@@ -0,0 +1,14 @@
+package Test::BDD::Cucumber::Definitions::TBCD::In;
+
+use strict;
+use warnings;
+use utf8;
+
+use Test::BDD::Cucumber::Definitions::HTTP::In;
+use Test::BDD::Cucumber::Definitions::Struct::In;
+use Test::BDD::Cucumber::Definitions::Var::In;
+use Test::BDD::Cucumber::Definitions::Zip::In;
+
+our $VERSION = '0.24';
+
+1;
